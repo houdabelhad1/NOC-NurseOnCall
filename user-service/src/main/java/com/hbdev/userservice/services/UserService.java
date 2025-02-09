8e2722a68dc82ca9a65638d1216835486b91b99b
houdabelhad1 <diyaenour75@gmail.com>
2025-02-09 10:38:35 +0100

    services creation

---
+++ user-service/src/main/java/com/hbdev/userservice/services/UserService.java
@@ -0,0 +1,47 @@
+package com.hbdev.userservice.services;
+
+import com.hbdev.userservice.dtos.UserDTO;
+import com.hbdev.userservice.dtos.UserRequestDTO;
+import com.hbdev.userservice.entities.Role;
+import com.hbdev.userservice.entities.User;
+import com.hbdev.userservice.repository.RoleRepository;
+import com.hbdev.userservice.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
+    public UserService(UserRepository userRepository, RoleRepository roleRepository) {
+        this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
+    }
+    public Optional<UserDTO> findByUsername(String username) {
+        return userRepository.findByUsername(username)
+                .map(this ::convertToDTO);
+    }
+
+    public Optional<UserDTO> findByEmail(String email) {
+        return userRepository.findByEmail(email)
+                .map(this ::convertToDTO);
+    }
+
+    public UserDTO save(UserRequestDTO userRequestDTO) {
+        User user = new User();
+        user.setUsername(userRequestDTO.getUsername());
+        user.setEmail(userRequestDTO.getUsername());
+        user.setPassword(userRequestDTO.getPassword());
+        User savedUser = userRepository.save(user);
+        return convertToDTO(savedUser);
+    }
+    public UserDTO convertToDTO(User user) {
+        UserDTO userDTO = new UserDTO();
+        userDTO.setUsername(user.getUsername());
+        userDTO.setPassword(user.getPassword());
+        userDTO.setRoles(user.getRoles().stream().map(Role::getName).collect(Collectors.toSet()));
+        return userDTO;
+    }
+}
