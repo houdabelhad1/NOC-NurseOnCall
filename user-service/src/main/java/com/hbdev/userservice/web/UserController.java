217e0474cb78bfb641026f071269290231430deb
houdabelhad1 <diyaenour75@gmail.com>
2025-02-09 22:52:01 +0100

    controller creation

---
+++ user-service/src/main/java/com/hbdev/userservice/web/UserController.java
@@ -0,0 +1,25 @@
+package com.hbdev.userservice.web;
+
+import com.hbdev.userservice.dtos.UserDTO;
+import com.hbdev.userservice.services.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+    private final UserService userService;
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+    @GetMapping("/username/{username}")
+    public ResponseEntity<UserDTO> getUserByUsername(@PathVariable String username) {
+        return userService.findByUsername(username)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+}
