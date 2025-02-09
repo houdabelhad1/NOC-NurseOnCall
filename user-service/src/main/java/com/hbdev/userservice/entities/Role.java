649bd33d1513a64ab5aa618a1f5af66219c9efed
houdabelhad1 <diyaenour75@gmail.com>
2025-02-09 01:04:29 +0100

    user & role entities creation

--- user-service/src/main/java/com/hbdev/userservice/entities/Role.java
+++ user-service/src/main/java/com/hbdev/userservice/entities/Role.java
@@ -1,4 +1,20 @@
 package com.hbdev.userservice.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
 }
--- user-service/src/main/java/com/hbdev/userservice/entities/User.java
+++ user-service/src/main/java/com/hbdev/userservice/entities/User.java
@@ -1,4 +1,24 @@
 package com.hbdev.userservice.entities;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String password;
+    private String email;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    private Set<Role> roles;
 }
